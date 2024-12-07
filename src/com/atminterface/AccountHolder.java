2d23de5e052e93f1bd7f6cf66c886b9d9477d82e
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-07 16:12:34 -0500

    Implement AccountHolder class wiht name, userId, and pin fields

--- src/com/atminterface/AccountHolder.java
+++ src/com/atminterface/AccountHolder.java
@@ -15,4 +15,21 @@ public class AccountHolder {
         //generate a userId automatically
         this.userId = UUID.randomUUID().toString().substring(0,8);
     }
+    public AccountHolder(String name, String userId, String pin) {
+        this.name = name;
+        this.userId = userId;
+        this.pin = pin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public boolean validatePin(String enteredPin) {
+        return this.pin.equals(enteredPin);
+    }
 }
