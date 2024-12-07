f6f771c029d0a112c489e1eb08dc1c6f8df8e8a4
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-07 16:21:10 -0500

    implement logic to encapsulate transaction details

--- src/com/atminterface/ATMMain.java
+++
@@ -1,4 +0,0 @@
-package com.atminterface;
-
-public class ATMMain {
-}
--- src/com/atminterface/BankTransaction.java
+++ src/com/atminterface/BankTransaction.java
@@ -1,4 +1,34 @@
 package com.atminterface;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class BankTransaction {
+    private double amount;
+    private String type; // "DEPOSIT", "WITHDRAW", "TRANSFER IN", "TRANSFER OUT"
+    private LocalDateTime timestamp;
+
+    public BankTransaction(double amount, String type) {
+        this.amount = amount;
+        this.type = type;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return timestamp.format(formatter);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getTimestamp() + "] " + type + ": " + amount;
+    }
 }
